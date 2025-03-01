072cd71800245b500ca684ac3a38e9f6371dc0d3
Kaleb <kalebzambrano@outlook.com>
2025-03-01 14:49:57 -0500

    adding exercises 3.3, 3.4, 3.5 and 3.6

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const persons = require("./persons.json")
+let persons = require("./persons.json")
 const express = require("express")
 
 const app = express()
 
 const PORT = process.env.PORT ?? 1234
+
+const generatedId = () =>{
+    const newId = Math.floor(Math.random()*3500)
+    return newId
+}
+
+app.use(express.json())
+app.disable('x-powered-by')
 
 app.get('/info', (req,res)=>{
     const fecha = new Date()
@@ -13,8 +21,49 @@ app.get('/info', (req,res)=>{
     )
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person){
+        return res.json(person)
+    } return res.status(404).send('<h1>404 not found</h1>')
+})
+
+app.delete('/api/persons/:id', (req, res)=>{
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 app.get('/api/persons', (req, res)=>{
     res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+    
+    const equalName = persons.find((person)=> person.name === req.body.name)
+
+    if (!req.body.name || !req.body.number) {
+        return res.status(400).json({ 
+          error: 'name or number missing' 
+        })
+    }else if(equalName){
+        return res.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    }
+
+    
+
+    const person = {
+        id: generatedId(),
+        name: req.body.name,
+        number: req.body.number
+    }
+
+    persons = persons.concat(person)
+    res.status(201).json(person)
 })
 
 app.use((req, res)=>{
