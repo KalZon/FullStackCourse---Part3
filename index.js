e7c5e9c7c352973493d6c983c2ae30f6914d5132
Kaleb <kalebzambrano@outlook.com>
2025-03-03 17:10:34 -0500

    adding Eslint and finishing exercise 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,75 +1,81 @@
-let persons = require("./persons.json")
-const express = require("express")
+let persons = require('./persons.json')
+const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 const PORT = process.env.PORT ?? 1234
 
-const generatedId = () =>{
-    const newId = Math.floor(Math.random()*3500)
-    return newId
+const generatedId = () => {
+  const newId = Math.floor(Math.random() * 3500)
+  return newId
 }
 
 app.use(express.json())
 app.disable('x-powered-by')
 
-app.get('/info', (req,res)=>{
-    const fecha = new Date()
-    res.send(
+app.use((req, res, next) => {
+  if (req.method === 'POST') {
+    morgan('tiny')(req, res, () => {})
+    console.log(` ${JSON.stringify(req.body)}`)
+  }
+  next()
+})
+
+app.get('/info', (req, res) => {
+  const fecha = new Date()
+  res.send(
         `<p>Phonebook has info for ${persons.length} people</p>
         <p>${fecha.toString()}<p>`
-    )
+  )
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person){
-        return res.json(person)
-    } return res.status(404).send('<h1>404 not found</h1>')
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    return res.json(person)
+  } return res.status(404).send('<h1>404 not found</h1>')
 })
 
-app.delete('/api/persons/:id', (req, res)=>{
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
 
-    res.status(204).end()
+  res.status(204).end()
 })
 
-app.get('/api/persons', (req, res)=>{
-    res.json(persons)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
 app.post('/api/persons', (req, res) => {
-    
-    const equalName = persons.find((person)=> person.name === req.body.name)
+  const equalName = persons.find((person) => person.name === req.body.name)
 
-    if (!req.body.name || !req.body.number) {
-        return res.status(400).json({ 
-          error: 'name or number missing' 
-        })
-    }else if(equalName){
-        return res.status(400).json({ 
-            error: 'name must be unique' 
-        })
-    }
+  if (!req.body.name || !req.body.number) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  } else if (equalName) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
 
-    
+  const person = {
+    id: generatedId(),
+    name: req.body.name,
+    number: req.body.number
+  }
 
-    const person = {
-        id: generatedId(),
-        name: req.body.name,
-        number: req.body.number
-    }
-
-    persons = persons.concat(person)
-    res.status(201).json(person)
+  persons = persons.concat(person)
+  res.status(201).json(person)
 })
 
-app.use((req, res)=>{
-    res.status(404).send('<h1>404 not found</h1>')  
+app.use((req, res) => {
+  res.status(404).send('<h1>404 not found</h1>')
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server listening on: http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`)
 })
